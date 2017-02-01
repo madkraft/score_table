568b8227d5eb397325fb190dd1a6efeffc5f7fa8
antonkostiuchkov <antonkostiuchkov@gmail.com>
2017-02-01 17:29:32 +0100

    added material design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { loadEngland } from './fetchService'
+
+import '../node_modules/material-design-lite/material.min.css'
+import '../node_modules/material-design-lite/material.min.js'
 import './App.css'
+
 
 class App extends Component {
   state = {
@@ -22,13 +26,24 @@ class App extends Component {
           <h2>{ this.state.epl.leagueCaption }</h2>
           <p>Matchday: { this.state.epl.matchday }</p>
         </div>
-        <div>
-          <ul>
+        <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+          <thead>
+            <tr>
+              <th>Position</th>
+              <th className="mdl-data-table__cell--non-numeric">Team</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
             { this.state.epl.standing.map(team => (
-              <li>{team.position} {team.teamName} {team.points}</li>
+              <tr>
+                <td>{team.position}</td>
+                <td className="mdl-data-table__cell--non-numeric">{team.teamName}</td>
+                <td>{team.points}</td>
+              </tr>
             )) }
-          </ul>
-        </div>
+          </tbody>
+        </table>
       </div>
     )
   }
