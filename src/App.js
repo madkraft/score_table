cf7acf340488c48cf74bfded3c500d28bd3fcb9e
antonkostiuchkov <antonkostiuchkov@gmail.com>
2017-02-01 18:07:24 +0100

    material design + scss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import { loadEngland } from './fetchService'
 
-import '../node_modules/material-design-lite/material.min.css'
-import '../node_modules/material-design-lite/material.min.js'
-import './App.css'
+import './App.scss'
 
 
 class App extends Component {
   state = {
     epl: {
-      leagueCaption: 'Hello',
+      leagueCaption: '',
       standing: []
     }
   }
@@ -22,31 +20,59 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <div className='App-header'>
-          <h2>{ this.state.epl.leagueCaption }</h2>
-          <p>Matchday: { this.state.epl.matchday }</p>
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-tabs">
+          <header className="mdl-layout__header">
+            <div className="mdl-layout__header-row">
+              <span className="mdl-layout-title">Title</span>
+            </div>
+            <div className="mdl-layout__tab-bar mdl-js-ripple-effect">
+              <a href="#fixed-tab-1" className="mdl-layout__tab is-active">Tab 1</a>
+              <a href="#fixed-tab-2" className="mdl-layout__tab">Tab 2</a>
+              <a href="#fixed-tab-3" className="mdl-layout__tab">Tab 3</a>
+            </div>
+          </header>
+          <div className="mdl-layout__drawer">
+            <span className="mdl-layout-title">Title</span>
+          </div>
+          <main className="mdl-layout__content">
+            <section className="mdl-layout__tab-panel is-active" id="fixed-tab-1">
+              <div className="page-content">hey</div>
+            </section>
+            <section className="mdl-layout__tab-panel" id="fixed-tab-2">
+              <div className="page-content">jello</div>
+            </section>
+            <section className="mdl-layout__tab-panel" id="fixed-tab-3">
+              <div className="page-content">wolrd</div>
+            </section>
+          </main>
         </div>
-        <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
-          <thead>
-            <tr>
-              <th>Position</th>
-              <th className="mdl-data-table__cell--non-numeric">Team</th>
-              <th>Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            { this.state.epl.standing.map(team => (
-              <tr>
-                <td>{team.position}</td>
-                <td className="mdl-data-table__cell--non-numeric">{team.teamName}</td>
-                <td>{team.points}</td>
-              </tr>
-            )) }
-          </tbody>
-        </table>
       </div>
     )
   }
 }
 
 export default App
+
+
+// <div className='App-header'>
+//           <h2>{ this.state.epl.leagueCaption }</h2>
+//           <p>Matchday: { this.state.epl.matchday }</p>
+//         </div>
+// <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+//   <thead>
+//     <tr>
+//       <th>Position</th>
+//       <th className="mdl-data-table__cell--non-numeric">Team</th>
+//       <th>Points</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     { this.state.epl.standing.map(team => (
+//       <tr>
+//         <td>{team.position}</td>
+//         <td className="mdl-data-table__cell--non-numeric">{team.teamName}</td>
+//         <td>{team.points}</td>
+//       </tr>
+//     )) }
+//   </tbody>
+// </table>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import '../node_modules/material-design-lite/material.min.css'
+import '../node_modules/material-design-lite/material.min.js'
+import './index.css'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
